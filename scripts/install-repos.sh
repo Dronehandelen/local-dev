e79d826890a8dad189db0636c40085a21b6dbad9
Max Moeschinger <max@momentech.no>
2021-03-07 19:14:36 +0100

    update repo location

--- scripts/install-repos.sh
+++ scripts/install-repos.sh
@@ -28,13 +28,13 @@ if [ ! -d "document-printer" ];then
 fi
 
 if [ ! -d "vipps-settlements" ];then
-  git clone git@bitbucket.org:norfpv/vipps-settlements.git
+  git clone git@github.com:Dronehandelen/vipps-settlements.git
 fi
 
 if [ ! -d "fiken" ];then
-  git clone git@bitbucket.org:norfpv/fiken.git
+  git clone git@github.com:Dronehandelen/fiken.git
 fi
 
 if [ ! -d "recommender" ];then
-  git clone git@bitbucket.org:norfpv/recommender.git
+  git clone git@github.com:Dronehandelen/recommender.git
 fi
